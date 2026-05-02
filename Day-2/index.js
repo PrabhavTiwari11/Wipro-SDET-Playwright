dcf895200b1990518446ee17320ef5c27fc7fe4a
PrabhavTiwari11 <tiwariprabhav143@gmail.com>
2026-05-02 09:56:17 +0530

    template , literals , concatenation ,Arrow Function

---
+++ Day-2/index.js
@@ -0,0 +1,43 @@
+let a = " Aryan";
+let b = "Prabhav";
+let c = "Tiwari"; // 3rd Variable
+
+let d = " Hello" + a + ", "+ b + " and "+ c+"!"; // Concatenation
+
+// console.log(d);
+
+let e = `Hello ${a}, ${b} and ${c}`; //Template Literals
+let f =`The length od a is ${a.length * 2}`;  //Using template literals to get length 
+//console.log(e);
+//console.log(f);
+
+function table(num){
+    let result = "";
+    for (let i =1;i<=10; i++){
+        // console.log(num + " x " + i + " = " + (num * i)); // Using string Concatenation
+        result += `${num} x ${i} = ${num * i}\n`;
+    }
+    return result;
+}
+// console.log(table(5)); // Displays the Multiplication table for 5
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// let sum1 = sum(5, 10);
+let sum = (x, y) => x + y; // Arrow function for Addition
+console.log("Sum of 5 and 10:", sum(5, 10)); // 15
+
+let arr = [1, 2, 3, 4, 5, "Aryan"];
+
+// arr.filter(function(x) {
+//     return typeof x === "number";
+// }).map(function(y) {
+//     return y * 2;
+// }).forEach(function(abc) {
+//     console.log("Element after * 2:", abc * 2);
+// });
+
+arr.filter(x => typeof x === "number").map(y => y * 2).forEach(abc => console.log("Element after * 2:", abc * 2));
+// console.log(arr.find(x => x === "Aryan1")); 
+// Arrow function is represented by =>
