0872a7beb96e1ffd541ebacca88b567d96e0e560
PrabhavTiwari11 <tiwariprabhav143@gmail.com>
2026-05-05 09:48:12 +0530

    Promise and Synchronous & Asynchronous Java Script

---
+++ Day-4/Index.js
@@ -0,0 +1,40 @@
+// Promise and Synchronous & Asynchronous Java Script 
+function getData() {
+
+ return new Promise((resolve, reject) => {
+
+ setTimeout(() => {
+
+ resolve("Data received");
+
+ }, 2000);
+
+ });
+
+}
+
+
+
+getData().then((data) => {
+
+ console.log(data);
+
+}).catch((error) => {
+
+ console.error("Error:", error);
+
+});
+
+// setTimeout(() => {
+
+// console.log("Data received");
+
+// }, 5000);
+
+
+
+// setInterval(() => {
+
+// console.log("Checking for new data...");
+
+// }, 3000);
