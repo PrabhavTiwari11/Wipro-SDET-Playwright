7a552b3a8d5d15e15e5f8dce0982e343dac36011
PrabhavTiwari11 <tiwariprabhav143@gmail.com>
2026-05-07 17:40:05 +0530

    Prototype Inheritance , Callback function, For each , Stack, Heap, Spread Operator

--- Day-6/PracticeQuestions.js
+++ Day-6/PracticeQuestions.js
@@ -39,6 +39,53 @@
 // ];
 
 
+const rawFlights = [
+"London-Paris:150",
+"Paris-Berlin:abc",
+"New York-Tokyo:invalid",
+"Dubai-Mumbai:450",
+"Berlin-Rome:95",
+"India-Dubai:501"
+];
+ 
+function cleanFlightData(flights) {
+    return flights.map(str => {
+        const parts = str.split(":");
+        const route = parts[0];
+        const price = parts[1];
+ 
+        const cities = route.split("-");
+        const from = cities[0];
+        const to = cities[1];
+ 
+        // console.log(from, to, price);
+        // if(typeof price === "undefined" || isNaN(Number(price))) {
+        //     return {
+        //         err: "Invalid price for route " + route
+        //     };
+        // }
+        return {
+            from: from,
+            to: to,
+            price: Number(price)
+        }
+    })
+}
+ 
+const cleanFlights = cleanFlightData(rawFlights);
+cleanFlights
+    .filter(f => {
+        if(typeof f.price === "undefined" || isNaN(Number(f.price))) {
+            f.price = 0
+        }
+    });
+const finalFlights = cleanFlights.filter(f => f.price >= 100 && f.price <= 500);
+finalFlights.sort((f1, f2) => f1.price - f2.price);
+finalFlights.forEach(e => console.log(e));
+console.log(finalFlights);
+
+
+//---------------------------------or------------------------------------
 
 
 const rawFlights = [
